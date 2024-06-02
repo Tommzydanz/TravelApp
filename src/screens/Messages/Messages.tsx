bef7c4f9f2bfa7b7d4447f97344ab41b14c1264a
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-06-02 13:12:52 +0100

    fix: added coming soon to later screens to be developed

--- src/screens/Messages/Messages.tsx
+++ src/screens/Messages/Messages.tsx
@@ -1,12 +1,20 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 
 const Messages = () => {
   return (
-    <View>
-      <Text>Messages</Text>
+    <View style={styles.root}>
+      <Text>Messages Coming Soon...</Text>
     </View>
   );
 };
 
 export default Messages;
+
+const styles = StyleSheet.create({
+  root: {
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

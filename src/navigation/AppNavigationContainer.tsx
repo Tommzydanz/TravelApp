5ad8028c3f8a39f5b613f755c9c1aeb933c09f53
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-06-02 13:09:01 +0100

    fix: nav container child component to home stack

--- src/navigation/AppNavigationContainer.tsx
+++ src/navigation/AppNavigationContainer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import TravelStack from './TravelStack';
+import TravelTabs from './TravelTabs';
+import HomeStack from './HomeStack';
 
 const AppNavigationContainer: React.FC<{}> = function AppNavigationContainer() {
   return (
     <NavigationContainer>
-      <TravelStack />
+      <HomeStack />
     </NavigationContainer>
   );
 };
